d50edfa1b451160af4f5fb642dd3ba5fdc9a88d4
Deepkaran Singh <dksinghbabbar@gmail.com>
2026-09-06 14:03:53 +0530

    Monitor Prefab setup |  Animator for button hover | Windows start script
    - time, date script linked
    - cursor anchor set to match the OS cursor perfectly
    - sprites setup.

---
+++ Assets/UI/ComputerSystem/WindowsStart.Time.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+namespace babbarversestudios
+{
+    public partial class WindowsStart : MonoBehaviour
+    {
+        [SerializeField] TMPro.TMP_Text timeRealtime;
+        [SerializeField] TMPro.TMP_Text timeRealtimeHome;
+        [SerializeField] TMPro.TMP_Text dateRealtimeHome;
+        private void SetTime()
+        {
+            timeRealtime.text = System.DateTime.Now.ToString("HH:mm");
+            timeRealtimeHome.text = System.DateTime.Now.ToString("HH:mm");
+        }
+
+        private void SetDate() =>
+            dateRealtimeHome.text = System.DateTime.Now.ToString("d");
+    }
+}
---
+++ Assets/UI/ComputerSystem/WindowsStart.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+namespace babbarversestudios
+{
+    public partial class WindowsStart : MonoBehaviour
+    {
+        [SerializeField] GameObject UnlockScreen;
+        [SerializeField] GameObject LockScreen;
+
+        public void UnlockWindows()
+        {
+            //TODO: We can use animator to beautify stuff
+            UnlockScreen.SetActive(true);
+            LockScreen.SetActive(false);
+        }
+
+        #region Unity Life Cycle
+        private void Start() =>
+            SetDate();
+
+        private void Update()
+        {
+            //needs to loop in this
+            SetTime();
+        }
+        #endregion
+    }
+}
